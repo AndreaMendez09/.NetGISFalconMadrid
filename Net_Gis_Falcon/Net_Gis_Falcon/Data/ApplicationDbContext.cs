314c5cceca2cf1564f45af12727aff145df6f747
Raul Barquero Consuegra <raul.barquero.external@atos.net>
2021-04-20 10:17:47 +0200

    Nuevo proyecto

---
+++ Net_Gis_Falcon/Net_Gis_Falcon/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Net_Gis_Falcon.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Net_Gis_Falcon/Net_Gis_Falcon/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Net_Gis_Falcon.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
