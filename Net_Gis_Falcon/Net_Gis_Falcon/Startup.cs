ddd8b54e82608cf6afe3f7f487cb3ed579761d80
Andrea Méndez Sanz <andrea.mendez.external@atos.net>
2021-04-20 17:42:48 +0200

    Añadimos dependencias

--- Net_Gis_Falcon/Net_Gis_Falcon/Startup.cs
+++ Net_Gis_Falcon/Net_Gis_Falcon/Startup.cs
@@ -35,6 +35,7 @@ namespace Net_Gis_Falcon
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+            //services.AddEntityFrameworkNpgsql
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
