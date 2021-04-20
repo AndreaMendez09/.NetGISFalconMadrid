99c9f28e3231bf7f4ca852f2a06c0934b82fef7d
Andrea Méndez Sanz <andrea.mendez.external@atos.net>
2021-04-20 10:35:52 +0200

    BBDD

---
+++ BBDD.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE "GIS_Falcon";
+DROP TABLE IF EXISTS PersonaSistema;
+DROP TABLE IF EXISTS Usuarios;
+DROP TABLE IF EXISTS Personas;
+
+CREATE TABLE Personas(
+    id_persona INTEGER PRIMARY KEY,
+    nombre varchar(20) not null,
+    apellido varchar(20) not null,
+    email varchar(20) not null,
+    genero char(5) not null,
+    idioma varchar(20) not null,
+    contraseña varchar(20)not null,
+    foto varchar(20)
+    );
+CREATE TABLE PersonaSistema(
+    rol INT,
+    zona VARCHAR(10)
+) INHERITS (Personas);
+
+CREATE TABLE Usuarios(
+    municipio VARCHAR(20),
+    fecha_nacimiento DATE
+
+) INHERITS (Personas);
