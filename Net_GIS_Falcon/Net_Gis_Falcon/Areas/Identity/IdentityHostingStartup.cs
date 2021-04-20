0ef7cc7cc04a5cfb3afd2647594432d8379af61a
Andrea Méndez Sanz <andrea.mendez.external@atos.net>
2021-04-20 13:12:49 +0200

    Vistas login/register full

---
+++ Net_GIS_Falcon/Net_Gis_Falcon/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Net_Gis_Falcon.Data;
+
+[assembly: HostingStartup(typeof(Net_Gis_Falcon.Areas.Identity.IdentityHostingStartup))]
+namespace Net_Gis_Falcon.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
