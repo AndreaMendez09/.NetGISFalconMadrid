af7e656eecb70536c8891ac356cd41201941cf4c
Andrea Méndez Sanz <andrea.mendez.external@atos.net>
2021-04-22 12:58:26 +0200

    Update TEST.sql

--- TEST.sql
+++ TEST.sql
@@ -1,7 +1,10 @@
+DROP TABLE Usuarios;
+
 CREATE TABLE Usuarios (
 	id_usuario INTEGER PRIMARY KEY,
-    nombre varchar(20) not null
+    nombre varchar(20) not null,
+	genero varchar(20) not null
 );
 
-INSERT INTO Usuarios VALUES (1, 'FUNCIONA');
+INSERT INTO Usuarios VALUES (1, 'FUNCIONA', 'HOLA');
 SELECT * FROM Usuarios;
