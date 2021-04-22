8fa5768063fc0faf48183ca25f04a31b2681aae7
Andrea Méndez Sanz <andrea.mendez.external@atos.net>
2021-04-22 11:12:01 +0200

    Creacion de modelo, BBDD test

---
+++ Net_GIS_Falcon/Net_Gis_Falcon/Models/UsuariosModelo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Net_Gis_Falcon.Models
+{
+    public class UsuariosModelo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ TEST.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Usuarios (
+	id_usuario INTEGER PRIMARY KEY,
+    nombre varchar(20) not null
+);
+
+INSERT INTO Usuarios VALUES (1, 'FUNCIONA');
+SELECT * FROM Usuarios;
