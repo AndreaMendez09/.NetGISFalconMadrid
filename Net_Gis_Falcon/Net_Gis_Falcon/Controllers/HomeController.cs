ca0c45f09e396ad2d0abd2bc21f434357b6db00c
Andrea Méndez Sanz <andrea.mendez.external@atos.net>
2021-04-22 14:07:14 +0200

    Lee la BBDD

--- Net_Gis_Falcon/Net_Gis_Falcon/Controllers/HomeController.cs
+++ Net_Gis_Falcon/Net_Gis_Falcon/Controllers/HomeController.cs
@@ -16,9 +16,10 @@ namespace Net_Gis_Falcon.Controllers
         private readonly ILogger<HomeController> _logger;
         private testContext Context { get; }
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(testContext _context)
         {
-            _logger = logger;
+            this.Context = _context;
         }
 
         public IActionResult Index()
@@ -28,35 +29,37 @@ namespace Net_Gis_Falcon.Controllers
 
         public IActionResult Privacy()
         {
-            //List<UsuariosModelo> usuarios = (from Usuarios in this.Context.Usuarios.Take(1) select Usuarios).ToList();
-            String connectionString = "Data Source=localhost;" +
-              "Initial Catalog=test;" +
-              "User id=postgres;" +
-              "Password=ADMIN;";
-            String sql = "SELECT * FROM Usuarios";
-            
+            List<Usuario> usuarios = (from Usuarios in this.Context.Usuarios.Take(1) select Usuarios).ToList();
+            /*String connectionString = "Data Source=localhost;" +
+               "Initial Catalog=test;" +
+               "User id=postgres;" +
+               "Password=ADMIN;";
+             String sql = "SELECT * FROM Usuarios";
 
-            var model = new List<Usuario>();
-            using (SqlConnection conn = new SqlConnection(connectionString))
-            {
-                SqlCommand cmd = new SqlCommand(sql, conn);
-                conn.Open();
-                SqlDataReader rdr = cmd.ExecuteReader();
-                while (rdr.Read())
-                {
-                    var usuario = new Usuario();
-                    Console.WriteLine(rdr["id_usuario"] + ",  " + rdr["nombre"]);
 
-                    //usuario.Id = rdr[""];
-                    //usuario.Name = rdr["nombre"];
-                    
-                    //model.Add(usuario);
-                }
-                rdr.Close();
+             var model = new List<Usuario>();
+             using (SqlConnection conn = new SqlConnection(connectionString))
+             {
+                 SqlCommand cmd = new SqlCommand(sql, conn);
+                 conn.Open();
+                 SqlDataReader rdr = cmd.ExecuteReader();
+                 while (rdr.Read())
+                 {
+                     var usuario = new Usuario();
+                     Console.WriteLine(rdr["id_usuario"] + ",  " + rdr["nombre"]);
 
-            }
+                     //usuario.Id = rdr[""];
+                     //usuario.Name = rdr["nombre"];
 
-            return View(model);
+                     //model.Add(usuario);
+                 }
+                 rdr.Close();
+
+             }*/
+
+            // return View(model);
+            return View(usuarios);
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
