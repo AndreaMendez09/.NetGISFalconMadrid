6784263782341c1ea8c2168b44205673bb54ca00
Leidy Alejandra Cortes Gonzalez <leidy.cortes.external@atos.net>
2021-05-06 12:49:58 +0200

    Peticiones y historico creado

---
+++ Net_GIS_Falcon/Net_Gis_Falcon/Controllers/HistoricoEstadoesController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Net_Gis_Falcon;
+
+namespace Net_Gis_Falcon.Controllers
+{
+    public class HistoricoEstadoesController : Controller
+    {
+        private readonly testContext _context;
+
+        public HistoricoEstadoesController(testContext context)
+        {
+            _context = context;
+        }
+
+        // GET: HistoricoEstadoes
+        public async Task<IActionResult> Index()
+        {
+            var testContext = _context.HistoricoEstados.Include(h => h.EstadoNavigation).Include(h => h.OperadorNavigation).Include(h => h.PeticionNavigation);
+            return View(await testContext.ToListAsync());
+        }
+
+        // GET: HistoricoEstadoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var historicoEstado = await _context.HistoricoEstados
+                .Include(h => h.EstadoNavigation)
+                .Include(h => h.OperadorNavigation)
+                .Include(h => h.PeticionNavigation)
+                .FirstOrDefaultAsync(m => m.Operador == id);
+            if (historicoEstado == null)
+            {
+                return NotFound();
+            }
+
+            return View(historicoEstado);
+        }
+
+        // GET: HistoricoEstadoes/Create
+        public IActionResult Create()
+        {
+            ViewData["Estado"] = new SelectList(_context.Estados, "IdEstado", "NombreEstado");
+            ViewData["Operador"] = new SelectList(_context.Usuarios, "IdUsuario", "Apellido");
+            ViewData["Peticion"] = new SelectList(_context.Peticions, "IdPeticion", "LocalizacionPeticion");
+            return View();
+        }
+
+        // POST: HistoricoEstadoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FechaModificacion,Estado,Peticion,Operador")] HistoricoEstado historicoEstado)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(historicoEstado);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Estado"] = new SelectList(_context.Estados, "IdEstado", "NombreEstado", historicoEstado.Estado);
+            ViewData["Operador"] = new SelectList(_context.Usuarios, "IdUsuario", "Apellido", historicoEstado.Operador);
+            ViewData["Peticion"] = new SelectList(_context.Peticions, "IdPeticion", "LocalizacionPeticion", historicoEstado.Peticion);
+            return View(historicoEstado);
+        }
+
+        // GET: HistoricoEstadoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var historicoEstado = await _context.HistoricoEstados.FindAsync(id);
+            if (historicoEstado == null)
+            {
+                return NotFound();
+            }
+            ViewData["Estado"] = new SelectList(_context.Estados, "IdEstado", "NombreEstado", historicoEstado.Estado);
+            ViewData["Operador"] = new SelectList(_context.Usuarios, "IdUsuario", "Apellido", historicoEstado.Operador);
+            ViewData["Peticion"] = new SelectList(_context.Peticions, "IdPeticion", "LocalizacionPeticion", historicoEstado.Peticion);
+            return View(historicoEstado);
+        }
+
+        // POST: HistoricoEstadoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("FechaModificacion,Estado,Peticion,Operador")] HistoricoEstado historicoEstado)
+        {
+            if (id != historicoEstado.Operador)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(historicoEstado);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HistoricoEstadoExists(historicoEstado.Operador))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Estado"] = new SelectList(_context.Estados, "IdEstado", "NombreEstado", historicoEstado.Estado);
+            ViewData["Operador"] = new SelectList(_context.Usuarios, "IdUsuario", "Apellido", historicoEstado.Operador);
+            ViewData["Peticion"] = new SelectList(_context.Peticions, "IdPeticion", "LocalizacionPeticion", historicoEstado.Peticion);
+            return View(historicoEstado);
+        }
+
+        // GET: HistoricoEstadoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var historicoEstado = await _context.HistoricoEstados
+                .Include(h => h.EstadoNavigation)
+                .Include(h => h.OperadorNavigation)
+                .Include(h => h.PeticionNavigation)
+                .FirstOrDefaultAsync(m => m.Operador == id);
+            if (historicoEstado == null)
+            {
+                return NotFound();
+            }
+
+            return View(historicoEstado);
+        }
+
+        // POST: HistoricoEstadoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var historicoEstado = await _context.HistoricoEstados.FindAsync(id);
+            _context.HistoricoEstados.Remove(historicoEstado);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HistoricoEstadoExists(int id)
+        {
+            return _context.HistoricoEstados.Any(e => e.Operador == id);
+        }
+    }
+}
---
+++ Net_GIS_Falcon/Net_Gis_Falcon/Controllers/PeticionsController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Net_Gis_Falcon;
+
+namespace Net_Gis_Falcon.Controllers
+{
+    public class PeticionsController : Controller
+    {
+        private readonly testContext _context;
+
+        public PeticionsController(testContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Peticions
+        public async Task<IActionResult> Index()
+        {
+            var testContext = _context.Peticions.Include(p => p.UsuarioNavigation);
+            return View(await testContext.ToListAsync());
+        }
+
+        // GET: Peticions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var peticion = await _context.Peticions
+                .Include(p => p.UsuarioNavigation)
+                .FirstOrDefaultAsync(m => m.IdPeticion == id);
+            if (peticion == null)
+            {
+                return NotFound();
+            }
+
+            return View(peticion);
+        }
+
+        // GET: Peticions/Create
+        public IActionResult Create()
+        {
+            ViewData["Usuario"] = new SelectList(_context.Usuarios, "IdUsuario", "Apellido");
+            return View();
+        }
+
+        // POST: Peticions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdPeticion,FechaCreacion,LocalizacionPeticion,Usuario")] Peticion peticion)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(peticion);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Usuario"] = new SelectList(_context.Usuarios, "IdUsuario", "Apellido", peticion.Usuario);
+            return View(peticion);
+        }
+
+        // GET: Peticions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var peticion = await _context.Peticions.FindAsync(id);
+            if (peticion == null)
+            {
+                return NotFound();
+            }
+            ViewData["Usuario"] = new SelectList(_context.Usuarios, "IdUsuario", "Apellido", peticion.Usuario);
+            return View(peticion);
+        }
+
+        // POST: Peticions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdPeticion,FechaCreacion,LocalizacionPeticion,Usuario")] Peticion peticion)
+        {
+            if (id != peticion.IdPeticion)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(peticion);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PeticionExists(peticion.IdPeticion))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Usuario"] = new SelectList(_context.Usuarios, "IdUsuario", "Apellido", peticion.Usuario);
+            return View(peticion);
+        }
+
+        // GET: Peticions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var peticion = await _context.Peticions
+                .Include(p => p.UsuarioNavigation)
+                .FirstOrDefaultAsync(m => m.IdPeticion == id);
+            if (peticion == null)
+            {
+                return NotFound();
+            }
+
+            return View(peticion);
+        }
+
+        // POST: Peticions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var peticion = await _context.Peticions.FindAsync(id);
+            _context.Peticions.Remove(peticion);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PeticionExists(int id)
+        {
+            return _context.Peticions.Any(e => e.IdPeticion == id);
+        }
+    }
+}
