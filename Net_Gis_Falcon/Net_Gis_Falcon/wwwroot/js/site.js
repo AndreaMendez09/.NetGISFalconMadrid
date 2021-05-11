3f4a9768a33926539d15e75fe3b565a0850283e8
Javitor15 <javier.garcia-duarte.external@atos.net>
2021-05-11 16:21:30 +0200

    Cogemos coordenadas de un punto marcado en el mapa

--- Net_Gis_Falcon/Net_Gis_Falcon/wwwroot/js/site.js
+++ Net_Gis_Falcon/Net_Gis_Falcon/wwwroot/js/site.js
@@ -4,29 +4,133 @@
 
 var source = new ol.source.Vector({ wrapX: false });
 
+source.on('addfeature', function (evt) {
+    var feature = evt.feature;
+    var coords = feature.getGeometry().getCoordinates();
+
+    console.log(coords);
+});
+
 var vector = new ol.layer.Vector({
     source: source,
 });
+var countryLayer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        url: './countries.geojson',
+        format: new ol.format.GeoJSON(),
+    }),
+    style: function (feature) {
+        style.getText().setText(feature.get('name'));
+        return style;
+    },
+});
 
 var map = new ol.Map({
-    layers: [raster, vector],
+    layers: [raster, vector, countryLayer],
     target: 'map',
     view: new ol.View({
-        center: [-11000000, 4600000],
-        zoom: 4,
+        center: [-412270.1806743107, 4926716.659837265],
+        zoom: 5,
     }),
+});
+
+var style = new ol.style.Style({
+    fill: new ol.style.Fill({
+        color: 'rgba(255, 255, 255, 0.6)',
+    }),
+    stroke: new ol.style.Stroke({
+        color: '#319FD3',
+        width: 1,
+    }),
+    text: new ol.style.Text({
+        font: '12px Calibri,sans-serif',
+        fill: new ol.style.Fill({
+            color: '#000',
+        }),
+        stroke: new ol.style.Stroke({
+            color: '#fff',
+            width: 3,
+        }),
+    }),
+});
+
+var highlightStyle = new ol.style.Style({
+    stroke: new ol.style.Stroke({
+        color: '#f00',
+        width: 1,
+    }),
+    fill: new ol.style.Fill({
+        color: 'rgba(255,0,0,0.1)',
+    }),
+    text: new ol.style.Text({
+        font: '12px Calibri,sans-serif',
+        fill: new ol.style.Fill({
+            color: '#000',
+        }),
+        stroke: new ol.style.Stroke({
+            color: '#f00',
+            width: 3,
+        }),
+    }),
+});
+
+var featureOverlay = new ol.layer.Vector({
+    source: new ol.source.Vector(),
+    map: map,
+    style: function (feature) {
+        highlightStyle.getText().setText(feature.get('name'));
+        return highlightStyle;
+    },
+});
+
+var highlight;
+var displayFeatureInfo = function (pixel) {
+    var feature = map.forEachFeatureAtPixel(pixel, function (feature) {
+        return feature;
+    });
+
+    var info = document.getElementById('info');
+    if (feature) {
+        info.innerHTML = feature.getId() + ': ' + feature.get('name');
+    } else {
+        info.innerHTML = '&nbsp;';
+    }
+
+    if (feature !== highlight) {
+        if (highlight) {
+            featureOverlay.getSource().removeFeature(highlight);
+        }
+        if (feature) {
+            featureOverlay.getSource().addFeature(feature);
+        }
+        highlight = feature;
+    }
+};
+
+map.on('pointermove', function (evt) {
+    if (evt.dragging) {
+        return;
+    }
+    var pixel = map.getEventPixel(evt.originalEvent);
+    displayFeatureInfo(pixel);
+});
+map.on('click', function (evt) {
+    displayFeatureInfo(evt.pixel);
 });
 
 var typeSelect = document.getElementById('type');
 
 var draw; // global so we can remove it later
+var geometryFunction;
 function addInteraction() {
     var value = typeSelect.value;
     if (value !== 'None') {
-        var geometryFunction;
         if (value === 'Square') {
             value = 'Circle';
             geometryFunction = ol.interaction.Draw.createRegularPolygon(4);
+        } else if (value === 'Point') {
+            value = 'Point';
+            geometryFunction = null;
         } else if (value === 'Box') {
             value = 'Circle';
             geometryFunction = ol.interaction.Draw.createBox();
@@ -54,6 +158,7 @@ function addInteraction() {
                 } else {
                     geometry.setCoordinates([newCoordinates]);
                 }
+
                 return geometry;
             };
         }
