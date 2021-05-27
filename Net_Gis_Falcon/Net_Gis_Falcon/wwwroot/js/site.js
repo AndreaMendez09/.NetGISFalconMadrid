98c965ba6ae8b5609ab723b9714b1397c5f53e49
Javitor15 <javier.garcia-duarte.external@atos.net>
2021-05-27 10:18:58 +0200

    Selección de capas (falta diseño de la página)

--- Net_Gis_Falcon/Net_Gis_Falcon/wwwroot/js/site.js
+++ Net_Gis_Falcon/Net_Gis_Falcon/wwwroot/js/site.js
@@ -1,5 +1,6 @@
 ﻿var baseMapLayer = new ol.layer.Tile({
     source: new ol.source.OSM(),
+    title: 'OSMBase'
 });
 
 var features = new ol.source.Vector({ wrapX: false });
@@ -10,26 +11,55 @@ var vector = new ol.layer.Vector({
 var countriesLayer = new ol.layer.Vector({
     source: new ol.source.Vector({
         //url: './georef-spain-municipio.json',    //Municipios (Demasiado grande, tarda mucho en cargar, no lo recomiendo)
-        //url: './countries.json',                 //Países
-        url: './provincias-espanolas.json',        //Provincias
+        url: './countries.json',                 //Países
+        //url: './provincias-espanolas.json',        //Provincias
         format: new ol.format.GeoJSON(),
     }),
     style: function (feature) {
         //style.getText().setText(feature.get('acom_name'));   //Municipios
-        //style.getText().setText(feature.get('name'));        //Países
-        style.getText().setText(feature.get('provincia'));   //Provincias
+        style.getText().setText(feature.get('name'));        //Países
+        //style.getText().setText(feature.get('provincia'));   //Provincias
         return style;
     },
+    title: 'OSMCountries',
+    visible: false
+});
+
+var mapQuestOSMLayer = new ol.layer.Tile({
+    source: new ol.source.OSM({
+        url: 'http://{a-c}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+    }),
+    visible: false,
+    title: 'OSMHumanitarian'
+});
+
+const baseLayerGroup = new ol.layer.Group({
+    layers: [baseMapLayer, mapQuestOSMLayer, countriesLayer]
 });
 
 var map = new ol.Map({
-    layers: [baseMapLayer, countriesLayer],
     target: 'map',
     view: new ol.View({
         center: [-412270.1806743107, 4926716.659837265],
         zoom: 5,
     }),
 });
+
+map.addLayer(baseLayerGroup)
+
+const baseLayerElements = document.querySelectorAll(".sidebar > input[type=radio]");
+
+for (let baseLayerElement of baseLayerElements) {
+    baseLayerElement.addEventListener('change', function () {
+        let baseLayerElementValue = this.value;
+        baseLayerGroup.getLayers().forEach(function (element, index, array) {
+            let baseLayerTitle = element.get('title');
+            element.setVisible(baseLayerTitle === baseLayerElementValue)
+            console.log(element.get('title'));
+        })
+    })
+}
+
 
 var style = new ol.style.Style({
     fill: new ol.style.Fill({
@@ -76,8 +106,8 @@ var featureOverlay = new ol.layer.Vector({
     map: map,
     style: function (feature) {
         //highlightStyle.getText().setText(feature.get('acom_name'));   //Municipios
-        //highlightStyle.getText().setText(feature.get('name'));        //Países
-        highlightStyle.getText().setText(feature.get('provincia'));     //´Provincias
+        highlightStyle.getText().setText(feature.get('name'));        //Países
+        //highlightStyle.getText().setText(feature.get('provincia'));     //´Provincias
         return highlightStyle;
     },
 });
@@ -91,8 +121,8 @@ var displayFeatureInfo = function (pixel) {
     var info = document.getElementById('info');
     if (feature) {
         //info.innerHTML = feature.get('acom_name');   //Municipios
-        //info.innerHTML = feature.get('name');        //Países
-        info.innerHTML = feature.get('provincia');     //Provincias
+        info.innerHTML = feature.get('name');        //Países
+        //info.innerHTML = feature.get('provincia');     //Provincias
     } else {
         info.innerHTML = '&nbsp;';
     }
