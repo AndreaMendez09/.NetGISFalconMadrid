c52dfa185141081a6610195841e0eabd10864597
Andrea Méndez Sanz <andrea.mendez.external@atos.net>
2021-06-01 12:32:00 +0200

    Migracion aleja

---
+++ Net_GIS_Falcon/Net_Gis_Falcon/Migrations/20210601103111_coordenadas_peticion.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Net_Gis_Falcon.Migrations
+{
+    public partial class coordenadas_peticion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "coordenadas",
+                table: "peticion",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "coordenadas",
+                table: "peticion");
+        }
+    }
+}
